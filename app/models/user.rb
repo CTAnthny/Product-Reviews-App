13be1ab5a21c7b7b5c86e0ad1d687eea4591386f
Chris Anthony <CTAnthny@gmail.com>
2017-03-09 20:40:25 -0500

    Fix user model password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,5 @@ class User < ApplicationRecord
   has_many :comments
   validates :first_name, presence: true, length: { in: 2..15 }
   validates :last_name, presence: true, length: { in: 2..25 }
-  validates :email, presence: true, uniqueness: true,
-    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: true
 end
