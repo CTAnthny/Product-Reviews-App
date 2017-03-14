8601c0db896c6da6c0569c28aca3aba5460dc731
Chris Anthony <CTAnthny@gmail.com>
2017-03-14 02:59:58 -0400

    Finish user creates account

--- spec/features/user_account/user_creates_account_spec.rb
+++ spec/features/user_account/user_creates_account_spec.rb
@@ -17,15 +17,32 @@ feature "user creates account" do
     fill_in 'First Name', with: 'John'
     fill_in 'Last Name', with: 'Smith'
     fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'user_password', with: 'password'
     fill_in 'Password Confirmation', with: 'password'
     click_button 'Sign Up'
 
-    expect(page).to have_content("Success!")
+    expect(page).to have_content("Welcome! You have signed up successfully.")
     expect(page).to have_content("Sign Out")
   end
 
-  scenario "required information is not supplied"
+  scenario "required information is not supplied" do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
-  scenario "password confirmation does not match"
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+  scenario "password confirmation does not match" do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'somethingdifferent'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 end
