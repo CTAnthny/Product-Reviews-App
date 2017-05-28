44ad541833572ccaf37b6ad2a993210742d122a8
Chris Anthony <CTAnthny@gmail.com>
2017-05-28 14:41:54 -0400

    add nested attributes validation product/comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  belongs_to :product, inverse_of: :comments
+
+  validates_presence_of :product
   validates :description, presence: true, length: { in: 10..1000 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, inverse_of: :product
+  accepts_nested_attributes_for :comments
+
   validates :name, presence: true, uniqueness: true, length: { in: 3..75 }
   validates :description, presence: true, length: { maximum: 1000 }
+
   paginates_per 15
 end
